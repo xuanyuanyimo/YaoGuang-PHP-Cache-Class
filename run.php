612bfed79c9f4e343177411b58af2dbed21c1b33
玄元一墨 <98516664+xuanyuanyimo@users.noreply.github.com>
2024-08-03 14:58:50 +0800

    Update run.php

--- run.php
+++ run.php
@@ -1,8 +1,22 @@
 <?php
     require_once "./src/class_cache.php";
 
-    $obj = new YaoGuang\cache();
-    // var_dump($obj->cache_get_data("test"));
-    // var_dump($obj->set_cache("test",array("测试字段A"=>"测试字段值 a","测试字段B"=>"测试字段值 b")));
-    // var_dump($obj->cache_is_overtime("test"));
-?>
+    // 实例化缓存类
+    $cache = new cache();
+    
+    // 缓存名
+    $cache_name = "test";
+    // 缓存数据
+    $cache_data = array("name" => "xuanyuanyimo", "age" => 25);
+    // 缓存文件名称
+    $cache_file = "data.json";
+    // 缓存过期时间
+    $cache_update_time = 120;
+    
+    // 设置缓存
+    $cache->set_cache($cache_name, $cache_data, $cache_file, $cache_update_time);
+    
+    // 获取缓存
+    $get_cache_data = $cache->cache_static_get_data($cache_name, $cache_file);
+    
+    var_dump($get_cache_data);
